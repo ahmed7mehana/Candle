4e38f9f346b59ffd325ab982f233445d49f61e61
ahmed <ahmed7mehana@gmail.com>
2023-04-20 19:53:18 +0200

    my change

--- Components/Navba.jsx
+++ Components/Navba.jsx
@@ -1,156 +1,113 @@
-import { useSession } from "next-auth/react";
-import { useTheme } from "next-themes";
-import Link from "next/link";
-import React, { useState, useEffect } from "react";
-import { AiOutlineMenu, AiOutlineClose ,AiOutlineShopping ,AiOutlineKey, AiOutlineHeart,AiFillFire} from "react-icons/ai";
-import Footer from "./Footer";
-import Top from "./Top";
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import logo from '../public/img/logo.jpeg'
 
 const Navba = () => {
-  const{theme,setTheme}=useTheme()
-
   const [nav, setNav] = useState(false);
-  const [color, setColor] = useState("transparent");
-  const [textColor, setTextColor] = useState("white");
-  const [top, setTop] = useState(30);
+  const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState('#eee');
+  const [linkColor, setLinkColor] = useState('#000000');
+  const[show ,setshow]=useState(false)
+  const[show2 ,setshow2]=useState(true)
+  const[value ,setvalue]=useState("")
 
-  const{data:session}=useSession()
+  const [navdown, setnavdown] = useState(false);
+
+
   const handleNav = () => {
     setNav(!nav);
   };
+
   useEffect(() => {
-    const changeColor = () => {
-      if (window.scrollY >= 60) {
-        setColor("#333");
-        setTextColor("#333");
+    const handleShadow = () => {
+      if (window.scrollY >= 150) {
+        setShadow(true);
+
       } else {
-        setColor("transparent");
-        setTextColor("#333333");
+        setShadow(false);
+
       }
     };
-    window.addEventListener("scroll", changeColor);
+    window.addEventListener('scroll', handleShadow);
 
-  }, []);
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const handleScroll = () => {
-        const isTop = window.scrollY > 60;
-        setTop(isTop ? 0 : 30);
-      };
-
-      window.addEventListener("scroll", handleScroll);
-
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }
   }, []);
 
   return (
-<div>
-<div className=" hidden sm:contents  ">
-<Top />
-</div>
+    <div
+      style={{ backgroundColor: `${navBg}` }}
+      className={
+        shadow
+          ? 'fixed top-0 w-full h-16 shadow-xl z-[100] ease-in-out duration-300 '
+          : ' absolute top-0  w-full  h-16 z-[100]'
+      }
+    >
+      <div className='flex  items-center w-full h-full px-2 2xl:px-16'>
 
-<div
-style={{background:`${color}`, top:top}}
-className="fixed left-0 sm:dark:bg-black  bg-black  w-full z-10 ease-in duration-300 px-0 py-2 sm:px-10 sm:py-2  "
->
-<div className="max-w-[1240px] m-auto flex justify-between items-center p-1 sm:p-3 ">
-  <Link href="/">
-    <h1 className="font-bold text-[25px] text-white">
-    Baradise
-    </h1>
-  </Link>
-  <ul 
-   className="hidden sm:flex font-bold  text-white">
-    <li className="p-2  ">
-      <Link  className=" text-[#717fe0]" href="/" >Home</Link>
-    </li>
-    <li className="p-2">
-      <Link  className=" hover:text-[#717fe0] duration-500"  href="/Allproducts">Shop</Link>
-    </li>
+        <div className='flex items-center sm:justify-around md:justify-around justify-between w-full text-center'>
+          <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
+            <li className='ml-10 text-sm uppercase hover:border-b font-bold border-white-300'>
+              <Link href='/'>Home</Link>
+            </li>
 
+            <li className='ml-10 text-sm uppercase hover:border-b font-bold border-white-300'>
+              <Link href='/Courses'>Courses </Link>
+            </li>
 
-<li className="p-2">
-<Link  className=" hover:text-[#717fe0] duration-500"  href="/About">About</Link>
-</li>
-<li className="p-2">
-<Link  className=" hover:text-[#717fe0] duration-500"  href="/Contact">contact</Link>
-</li>
-<li className="p-2">
-<Link  className={session?" hidden":"  bg-white text-black rounded-3xl py-1 px-2   hover:bg-black hover:text-white font-bold duration-500"}  href="/login">logIn</Link>
-</li>
-<li className="p-2  flex items-center">
-<AiFillFire className=" dark:text-red-500 hover:text-white font-bold duration-500 " onClick={()=>{setTheme(theme==="dark"?"light":"dark")}}/>
-</li>
+          </ul>
 
-</ul>
+          <Link href='/' className='flex items-center'>
+          <span className='text-black text-sm sm:text-[20px]'>lamsa</span>
+          <Image src={logo} alt='img' className=' m-auto rounded-[50%] sm:w-full md:w-full w-[30%] relative sm:top-5'/>
+          <span className='text-black text-sm sm:text-[20px]'>candles</span>
+                  </Link>
 
+          <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
 
+            <li className='ml-10 text-sm uppercase hover:border-b font-bold border-white-300'>
+            <Link href='/About'> about us </Link>
+          </li>
 
-  
-  {/* Mobile Button */}
-  <div onClick={handleNav} className="block sm:hidden z-10">
-    {nav ? (
-      <AiOutlineClose size={20} className="!text-white" style={{ color: `${textColor}` }} />
-    ) : (
-      <AiOutlineMenu size={20} className="text-white" />
-    )}
-  </div>
+          <li className='ml-10 text-sm uppercase hover:border-b font-bold border-white-300'>
+          <Link href='' onClick={()=>{setshow(!show)}}> admin</Link>
 
-  {/* Mobile Menu */}
+{show?(
+  <div className=' absolute top-16 right-4 flex flex-col'> 
 
-
-  <div
-    className={
-      nav
-        ? "sm:hidden absolute top-[60px]  left-0  bottom-0  justify-center flex flex-wrap flex-col  items-center w-[250px] rounded h-max bg-[#717fe0] text-center ease-in duration-300"
-        : "sm:hidden absolute top-[60px]  left-[-100%]  bottom-0  justify-center flex flex-wrap flex-col items-center w-[250px] rounded h-max bg-[#717fe0] text-center ease-in duration-300"
+  <input type='text' placeholder='Passwork' className='p-2 rounded' value={value}  onChange={(e)=>{setvalue(e.target.value)}} />
+  <button onClick={()=>{
+    if(value==="123456789"){
+      setshow2(!show2)
     }
-  >
+  }} className='bg-black p-2 rounded text-[#f78da7]'>ok</button>
 
-    <ul className=" text-white ">
-    <li
-    onClick={handleNav}
-    className="p-4 text-4xl hover:text-gray-500"
-  >
-  <Link  href="/account" >my account</Link>
-  </li>
-      <li
-        onClick={handleNav}
-        className="p-4 text-4xl hover:text-gray-500"
-      >
-      <Link  href="/" >Home</Link>
-      </li>
-      <li
-        onClick={handleNav}
-        className="p-4 text-4xl hover:text-gray-500"
-      >
-      <Link href="/Allproducts">Shop</Link>
-      </li>
+  {show2?"":(
 
-
-
-  <li
-  onClick={handleNav}
-  className="p-4 text-4xl hover:text-gray-500"
->
-<Link href="/About">About</Link>
-</li>
-<li
-onClick={handleNav}
-className="p-4 text-4xl hover:text-gray-500"
->
-<Link href="/Contact">contact</Link>
-</li>
-
-    </ul>
+    <Link href='/Dashboard' className='text-black p-2 rounded bg-[#f78da7]'  onClick={()=>{setshow(!show),setvalue(""),setshow2(!show2)}}> lets go</Link>
+  )}
   </div>
+):""}
+        </li>
 
-</div>
-</div>
-</div>
+          </ul>
+
+
+          {/* Hamburger Icon */}
+          <div
+            style={{ color: `${linkColor}` }}
+            onClick={handleNav}
+            className='md:hidden'
+          >
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+
+    </div>
   );
 };
 
